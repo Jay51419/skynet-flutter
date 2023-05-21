b43af27ca2959619829e4de463b51dbb5a431d68
jay51419 <jaygandhi51419@gmail.com>
2023-05-21 16:10:06 +0530

    remove unused code

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,7 @@
-import 'package:flashy_tab_bar2/flashy_tab_bar2.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:skynet/login/login_page.dart';
-import 'package:skynet/profile/profile_page.dart';
-import 'package:skynet/support/support_page.dart';
-import 'package:url_launcher/url_launcher.dart';
 import 'app_state.dart';
-import 'package:skynet/recharge/recharge_page.dart';
-import 'package:skynet/theme.dart';
-import 'home/home_page.dart';
 
 void main() {
   runApp(const AppState(child: MyApp()));
--- lib/profile/profile_page.dart
+++ lib/profile/profile_page.dart
@@ -18,85 +18,83 @@ class ProfilePage extends StatelessWidget {
       ),
       child: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const DetailTab(name: "Username", data: "Meet01"),
-              const DetailTab(name: "Name", data: "Meet Joshi"),
-              const DetailTab(name: "ID", data: "atfvevveh826b2nihb"),
-              const DetailTab(name: "Account No.", data: "554444"),
-              ButtonDetailTab(
-                name: "Change Password",
-                onTap: () {
-                  Navigator.of(context)
-                      .push(MaterialPageRoute(builder: (context) {
-                    return const ChangeWifiPasswordPage();
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const DetailTab(name: "Username", data: "Meet01"),
+            const DetailTab(name: "Name", data: "Meet Joshi"),
+            const DetailTab(name: "ID", data: "atfvevveh826b2nihb"),
+            const DetailTab(name: "Account No.", data: "554444"),
+            ButtonDetailTab(
+              name: "Change Password",
+              onTap: () {
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (context) {
+                  return const ChangeWifiPasswordPage();
+                }));
+              },
+            ),
+            const DetailTab(name: "Status", data: "Active"),
+            DropDownDetailTab(
+              name: "Address",
+              dropdownWidget: Text(
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an",
+                style: GoogleFonts.poppins(color: Colors.black38),
+              ),
+            ),
+            const DetailTab(name: "Contact", data: "9856445699"),
+            const DetailTab(name: "Creation Date", data: "20 Nov 2022"),
+            const DetailTab(name: "Renewal Date", data: "20 Nov 2022"),
+            const DetailTab(name: "Expire Date", data: "20 Nov 2022"),
+            DropDownDetailTab(
+              name: "Plan",
+              dropdownWidget: Container(
+                padding: const EdgeInsets.all(8),
+                margin: const EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(color: Colors.black12, width: 0.5)),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "₹ 1800",
+                      style: GoogleFonts.poppins(
+                          fontWeight: FontWeight.bold, fontSize: 24),
+                    ),
+                    Text(
+                      "Unlimted Data @100 Mbps with 90 days Validity",
+                      style: GoogleFonts.poppins(color: Colors.black38),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              alignment: Alignment.center,
+              child: TextButton(
+                style: ButtonStyle(
+                  alignment: Alignment.center,
+                  fixedSize: MaterialStateProperty.resolveWith(
+                      (states) => const Size(84, 40)),
+                  foregroundColor: MaterialStateColor.resolveWith(
+                      (states) => Colors.red.shade200),
+                  overlayColor: MaterialStateColor.resolveWith(
+                      (states) => Colors.black12),
+                ),
+                onPressed: () {
+                  Navigator.pushReplacement(context,
+                      MaterialPageRoute(builder: (context) {
+                    return const LoginPage();
                   }));
                 },
-              ),
-              const DetailTab(name: "Status", data: "Active"),
-              DropDownDetailTab(
-                name: "Address",
-                dropdownWidget: Text(
-                  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an",
-                  style: GoogleFonts.poppins(color: Colors.black38),
+                child: const Row(
+                  children: [Icon(Icons.logout), Text("Logout")],
                 ),
               ),
-              const DetailTab(name: "Contact", data: "9856445699"),
-              const DetailTab(name: "Creation Date", data: "20 Nov 2022"),
-              const DetailTab(name: "Renewal Date", data: "20 Nov 2022"),
-              const DetailTab(name: "Expire Date", data: "20 Nov 2022"),
-              DropDownDetailTab(
-                name: "Plan",
-                dropdownWidget: Container(
-                  padding: const EdgeInsets.all(8),
-                  margin: const EdgeInsets.all(8),
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      border: Border.all(color: Colors.black12, width: 0.5)),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "₹ 1800",
-                        style: GoogleFonts.poppins(
-                            fontWeight: FontWeight.bold, fontSize: 24),
-                      ),
-                      Text(
-                        "Unlimted Data @100 Mbps with 90 days Validity",
-                        style: GoogleFonts.poppins(color: Colors.black38),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              Container(
-                width: double.infinity,
-                alignment: Alignment.center,
-                child: TextButton(
-                  style: ButtonStyle(
-                    alignment: Alignment.center,
-                    fixedSize: MaterialStateProperty.resolveWith(
-                        (states) => const Size(84, 40)),
-                    foregroundColor: MaterialStateColor.resolveWith(
-                        (states) => Colors.red.shade200),
-                    overlayColor: MaterialStateColor.resolveWith(
-                        (states) => Colors.black12),
-                  ),
-                  onPressed: () {
-                    Navigator.pushReplacement(context,
-                        MaterialPageRoute(builder: (context) {
-                      return const LoginPage();
-                    }));
-                  },
-                  child: const Row(
-                    children: [Icon(Icons.logout), Text("Logout")],
-                  ),
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
